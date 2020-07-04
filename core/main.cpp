6073031fd0614a75b06537e48b86b0e7fefc6b19
Perdigigio <51318916+Perdigigio@users.noreply.github.com>
2020-07-04 13:58:58 +0200

    Update main.cpp
    disable GL_KHR_DEBUG

--- core/main.cpp
+++ core/main.cpp
@@ -130,7 +130,6 @@ int main()
 
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
 
 	g_Window = glfwCreateWindow(SCREEN_W, SCREEN_H, "Morph", NULL, NULL);
 
@@ -183,19 +182,6 @@ int main()
 
 		return (glfwDestroyWindow(g_Window), glfwTerminate(), -1);
 	}
-
-	glEnable(GL_DEBUG_OUTPUT_KHR);
-	glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS_KHR);
-
-	glDebugMessageCallbackKHR([](GLenum source,GLenum type,GLuint id,GLenum severity,GLsizei length,const GLchar *message,const void *userParam)
-	{
-		if (severity == GL_DEBUG_SEVERITY_HIGH_KHR ||
-			severity == GL_DEBUG_SEVERITY_MEDIUM_KHR ||
-			severity == GL_DEBUG_SEVERITY_LOW_KHR)
-		{
-			LOG_ERR(id, message);
-		}
-	}, NULL);
 
 	ImGui::CreateContext();
 
